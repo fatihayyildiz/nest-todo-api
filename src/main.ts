751b898c381238df715ecad00693385624395a2c
fatihayyildiz <fatihhikmetayyildiz@gmail.com>
2021-11-23 00:14:25 +0100

    port typo fixed

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8030);
+  await app.listen(process.env.PORT || 8030);
 }
 bootstrap();
