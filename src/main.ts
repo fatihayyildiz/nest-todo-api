61cf38e4459e95a2e853ce2b8a7ba4cf8632bc94
Fatih Ayyildiz <fatihhikmetayyildiz@gmail.com>
2021-12-17 17:31:14 +0100

    swagger iu link changed
    https://todo-postgres-api.herokuapp.com/swagger_new

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger_new', app, document);
 
   await app.listen(process.env.PORT || 8030);
 }
